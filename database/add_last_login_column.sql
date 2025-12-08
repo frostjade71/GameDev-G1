f1cbd46c97b0aabc6b2b6205dfd2380d32041065
frostjade71 <jaderbypenaranda@gmail.com>
2025-12-08 21:51:07 +0800

    security: Remove database for security

--- database/add_last_login_column.sql
+++
@@ -1,9 +0,0 @@
--- Add last_login column to users table
--- Run this SQL in phpMyAdmin to add the last_login tracking column
-
-ALTER TABLE `users` 
-ADD COLUMN `last_login` DATETIME NULL DEFAULT NULL AFTER `updated_at`;
-
--- Optional: Add an index for better query performance
-ALTER TABLE `users` 
-ADD INDEX `idx_last_login` (`last_login`);
